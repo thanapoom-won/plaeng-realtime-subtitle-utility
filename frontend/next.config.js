4b9dc5f399bcc209f08b4637c36a093997cd4ad8
Thanapoom <6230228121@student.chula.ac.th>
2023-04-04 23:50:04 +0700

    [ADD] security policy header

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'Permissions-Policy',
+    value: 'microphone=(self)'
+  }
+]
 const nextConfig = {
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
